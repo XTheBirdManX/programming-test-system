0ba50f9ac48f54be60af75fda777e0f6c0f4616b
StanDoge <stanlydoge@gmail.com>
2022-09-24 16:33:49 -0600

    First question

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	string level;
+	string sector;
+
+	// Inputs
+	cout << "------------------------------------------------------------------------------------------" << endl;
+	cout << "Bienvenido/a a tu mix de lenguajes de programcion" << endl;
+	cout << "Tenemos unas preguntas para ti, esto con la finalidad de tener una mejor seleccion para ti" << endl;
+	cout << "------------------------------------------------------------------------------------------" << endl;
+
+	cout << "\n01. ¿Que nivel de conocimiento tienes en el mundo de la programacion?" << endl;
+	cout << "\na) Noob\n"
+			 << "\nb) Principiante\n"
+			 << "\nc) Intermedio\n"
+			 << "\nd) Avanzado\n"
+			 << "\ne) Pro\n"
+			 << endl;
+	cin >> level;
+
+	cout << "02. ¿En que sector te gustaria trabajar?" << endl;
 	return 0;
 }
