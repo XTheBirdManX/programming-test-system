52bd15d764673723cc0b5627a85d4033460c92e8
StanDoge <StanlyDoge@gmail.com>
2022-10-08 22:57:19 -0600

    Adicion de nodos adyacentes

--- src/graph.cpp
+++ src/graph.cpp
@@ -8,5 +8,24 @@ using namespace std;
 typedef struct Nodo{
 	map<string,vector<Nodo>> adyacentes;
 	string nombre, area;
-	string recurso[];
+	string recurso[3];
 }Lenguaje;
+
+void add(Lenguaje *nodoBase,vector<Lenguaje> nodosAdyacentes,string nodoPadre){
+  nodoBase->adyacentes.insert({nodoPadre,nodosAdyacentes}); 
+	cout << "Nodo agregado" << endl;
+	for (auto it = nodoBase->adyacentes.begin(); it != nodoBase->adyacentes.end(); ++it){
+		cout << it->first << endl;
+		for (int i = 0; i < it->second.size(); i++){
+			cout << it->second[i].nombre << endl;
+		}
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	Lenguaje miNodo,primero,segundo,tercero;
+	vector<Lenguaje> nodos = {primero,segundo,tercero};
+	add(&miNodo,nodos,"nodoPadre");
+	return 0;
+}
