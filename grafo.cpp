680b6130676333f9cb3174250a1352ff2d881fdc
StanDoge <stanlydoge@gmail.com>
2022-10-25 14:59:59 -0600

    Sin propmt del error

--- grafo.cpp
+++ grafo.cpp
@@ -1,74 +1,77 @@
-//#include <iostream>
-//#include <string>
-//#include <map>
-//#include <vector>
-//
-//using namespace std;
-//
-//typedef struct Nodo
-//{
-//	map<string, vector<Nodo>> adyacentes;
-//	string nombre, area;
-//	string recursos;
-//	string recursosWeb[3] = {"HTRML", "CSS", "JS"};
-//	string recursosMovil[3] = {"Android", "IOS", "Windows"};
-//	string recursosVideojuegos[3] = {"Windows", "Linux", "Mac"};
-//} Lenguaje;
-//
-//void agregar(Lenguaje *nodoBase, vector<Lenguaje> nodosAdyacentes, string nodoPadre);
-//Lenguaje inicializar(string nombreLenguaje, string area);
-//
-//int main(int argc, char const *argv[])
-//{
-//	Lenguaje miNodo;
-//	Lenguaje primero = inicializar("JavaScript", "Web");
-//	Lenguaje segundo = inicializar("CSharp", "Videojuegos");
-//	Lenguaje tercero = inicializar("Java", "Movil");
-//	Lenguaje cuarto = inicializar("Cuarto", "Otro");
-//	vector<Lenguaje> nodos = {primero, segundo, tercero};
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+typedef struct Nodo
+{
+	map<string, vector<Nodo> > adyacentes;
+	string nombre, area;
+	string recursos;
+	string recursosWeb[3] = {"HTRML", "CSS", "JS"};
+	string recursosMovil[3] = {"Android", "IOS", "Windows"};
+	string recursosVideojuegos[3] = {"Windows", "Linux", "Mac"};
+} Lenguaje;
+
+void agregar(Lenguaje *nodoBase, vector<Lenguaje> nodosAdyacentes, string nodoPadre);
+Lenguaje inicializar(string nombreLenguaje, string area);
+
+int main(int argc, char const *argv[])
+{
+	Lenguaje miNodo;
+	vector<Lenguaje> nodos;
+	Lenguaje primero = inicializar("JavaScript", "Web");
+	Lenguaje segundo = inicializar("CSharp", "Videojuegos");
+	Lenguaje tercero = inicializar("Java", "Movil");
+	Lenguaje cuarto = inicializar("Cuarto", "Otro");
+	nodos.push_back(primero);
+//	vector<Lenguaje> nodos{primero, segundo, tercero};
 //	agregar(&miNodo, nodos, "nodoPadre");
-//	return 0;
-//}
-//
-//// Aca deberia deberia de ir una validacion para que previo a la comparacion de string se quiten los espacios en blanco y se conviertan a minusculas/mayusculas para comparar.
-//
-///// @brief De uso interno, permite "quemar" los nodos que posteriomente seran recorridos para seleccionar uno.
-///// @nombreLenguaje  Nombre del lenguaje.
-///// @area Area a la que pertenece el lenguaje.
-///// @return Un nodo con sus atributos inicializados. 
-//Lenguaje inicializar(string nombreLenguaje, string area)
-//{
-//	Lenguaje nuevoNodo = {
-//			.nombre = nombreLenguaje,
-//			.area = area};
-//	if (nuevoNodo.area == "Web")
-//	{
-//		// nuevoNodo.recursos = nuevoNodo.recursosWeb;
-//		cout << "Aqui deberia de inicilarse el nodo con recursos web" << endl;
-//	}
-//	else if (area == "Movil")
-//	{
-//		// nuevoNodo.recursos = nuevoNodo.recursosMovil;
-//		cout << "Aqui deberia dejarse recursos movil" << endl;
-//	}
-//	else if (area == "Videojuegos")
-//	{
-//		// nuevoNodo.recursos = nuevoNodo.recursosVideojuegos;
-//		cout << "Aqui deberia dejarse recursos de Videojuegos" << endl;
-//	}
-//	else
-//	{
-//		// string foo[3] = {"Si", "NO", "Porque"};
-//		// nuevoNodo.recursos = foo;
-//		cout << "else" << endl;
-//	}
-//	return nuevoNodo;
-//}
-//
-///// @brief Agrega los nodos adyacentes al nodo base, ademas los muestra
-///// @param nodoBase
-///// @param nodosAdyacentes
-///// @param nodoPadre
+	return 0;
+}
+
+// Aca deberia deberia de ir una validacion para que previo a la comparacion de string se quiten los espacios en blanco y se conviertan a minusculas/mayusculas para comparar.
+
+/// @brief De uso interno, permite "quemar" los nodos que posteriomente seran recorridos para seleccionar uno.
+/// @nombreLenguaje  Nombre del lenguaje.
+/// @area Area a la que pertenece el lenguaje.
+/// @return Un nodo con sus atributos inicializados. 
+Lenguaje inicializar(string nombreLenguaje, string area)
+{
+	Lenguaje nuevoNodo;
+	nuevoNodo.area = area;
+	nuevoNodo.nombre = nombreLenguaje;
+	
+	if (nuevoNodo.area == "Web")
+	{
+		// nuevoNodo.recursos = nuevoNodo.recursosWeb;
+	}
+	else if (area == "Movil")
+	{
+		// nuevoNodo.recursos = nuevoNodo.recursosMovil;
+		cout << "Aqui deberia dejarse recursos movil" << endl;
+	}
+	else if (area == "Videojuegos")
+	{
+		// nuevoNodo.recursos = nuevoNodo.recursosVideojuegos;
+		cout << "Aqui deberia dejarse recursos de Videojuegos" << endl;
+	}
+	else
+	{
+		// string foo[3] = {"Si", "NO", "Porque"};
+		// nuevoNodo.recursos = foo;
+		cout << "else" << endl;
+	}
+	return nuevoNodo;
+}
+
+/// @brief Agrega los nodos adyacentes al nodo base, ademas los muestra
+/// @param nodoBase
+/// @param nodosAdyacentes
+/// @param nodoPadre
+
 //void agregar(Lenguaje *nodoBase, vector<Lenguaje> nodosAdyacentes, string nodoPadre)
 //{
 //	nodoBase->adyacentes.insert({nodoPadre, nodosAdyacentes});
--- main.cpp
+++ main.cpp
@@ -1,68 +1,68 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-bool esValida(string); // Implentar las validaciones
-
-int main(int argc, char const *argv[])
-{
-
-	string nivel;
-	string sector;
-
-	// Inputs
-	cout << "------------------------------------------------------------------------------------------" << endl;
-	cout << "		? Bienvenido/a a tu mix de lenguajes de programcion ? " << endl;
-	cout << "Tenemos unas preguntas para ti, esto con la finalidad de tener una mejor seleccion para ti ??" << endl;
-	cout << "------------------------------------------------------------------------------------------" << endl;
-
-	cout << "\n01. ¿Que nivel de conocimiento tienes en el mundo de la programacion?" << endl;
-	cout << "\na) Noob\n"
-			 << "\nb) Principiante\n"
-			 << "\nc) Intermedio\n"
-			 << "\nd) Avanzado\n"
-			 << "\ne) Pro\n"
-			 << endl;
-	getline(cin, nivel);
-	esValida(nivel);
-	cin >> nivel;
-
-	system("cls");
-//	system("clear");
-	cout << "\n02. ¿En que sector te gustaria trabajar?" << endl;
-
-	cout << "\na) Web\n"
-			 << "\nb) Movil\n"
-			 << "\nc) Videojuegos\n"
-			 << endl;
-	getline(cin, sector);
-	esValida(sector);
-	cin >> sector;
-
-	return 0;
-}
-
-/// @brief Valida la entrada de la variable "area" del usuario sea valida.
-/// @param entrada Entrada del usuario capturada mediante consola.
-/// @return Un boolean que expresa si es o no valida.
-bool esValida(string x)
-//cambia el nombre de x -> entrada, es mas intuitivo ese nombre de variable. Despues de hacerlo borras este comentario.
-{
-	int i = 0;
-	char c;
-	for (i; i < x.size(); i++)
-	{
-		c = x[i];
-		if (isalpha(c) == false)
-			if (isspace(c) == false)
-			{
-				{
-					cout << "El caracter: " << c << " No es valido\n"
-							 << "ingrese solo letras" << endl;
-					return false;
-				}
-			}
-	}
-}
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//bool esValida(string); // Implentar las validaciones
+//
+//int main(int argc, char const *argv[])
+//{
+//
+//	string nivel;
+//	string sector;
+//
+//	// Inputs
+//	cout << "------------------------------------------------------------------------------------------" << endl;
+//	cout << "		? Bienvenido/a a tu mix de lenguajes de programcion ? " << endl;
+//	cout << "Tenemos unas preguntas para ti, esto con la finalidad de tener una mejor seleccion para ti ??" << endl;
+//	cout << "------------------------------------------------------------------------------------------" << endl;
+//
+//	cout << "\n01. ¿Que nivel de conocimiento tienes en el mundo de la programacion?" << endl;
+//	cout << "\na) Noob\n"
+//			 << "\nb) Principiante\n"
+//			 << "\nc) Intermedio\n"
+//			 << "\nd) Avanzado\n"
+//			 << "\ne) Pro\n"
+//			 << endl;
+//	getline(cin, nivel);
+//	esValida(nivel);
+//	cin >> nivel;
+//
+//	system("cls");
+////	system("clear");
+//	cout << "\n02. ¿En que sector te gustaria trabajar?" << endl;
+//
+//	cout << "\na) Web\n"
+//			 << "\nb) Movil\n"
+//			 << "\nc) Videojuegos\n"
+//			 << endl;
+//	getline(cin, sector);
+//	esValida(sector);
+//	cin >> sector;
+//
+//	return 0;
+//}
+//
+///// @brief Valida la entrada de la variable "area" del usuario sea valida.
+///// @param entrada Entrada del usuario capturada mediante consola.
+///// @return Un boolean que expresa si es o no valida.
+//bool esValida(string x)
+////cambia el nombre de x -> entrada, es mas intuitivo ese nombre de variable. Despues de hacerlo borras este comentario.
+//{
+//	int i = 0;
+//	char c;
+//	for (i; i < x.size(); i++)
+//	{
+//		c = x[i];
+//		if (isalpha(c) == false)
+//			if (isspace(c) == false)
+//			{
+//				{
+//					cout << "El caracter: " << c << " No es valido\n"
+//							 << "ingrese solo letras" << endl;
+//					return false;
+//				}
+//			}
+//	}
+//}
 
